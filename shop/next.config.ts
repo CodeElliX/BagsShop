63f7d4a15f11f9263297c97c343e2db87bd460f0
ElliX <codeellix@gmail.com>
2025-03-08 14:41:41 +0200

    fix: delete base path

--- shop/next.config.ts
+++ shop/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/BagsShop",
-  assetPrefix: "/BagsShop/",
+  // basePath: "/BagsShop",
+  // assetPrefix: "/BagsShop/",
   trailingSlash: true,
   images: {
     remotePatterns: [

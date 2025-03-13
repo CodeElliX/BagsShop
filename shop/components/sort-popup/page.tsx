1c1cdc37ffb42e81596eecc52cf65236c65617dc
ElliX <codeellix@gmail.com>
2025-03-13 21:30:15 +0200

    feat: added media screen for catalog-settings

--- shop/components/sort-popup/page.tsx
+++ shop/components/sort-popup/page.tsx
@@ -48,16 +48,8 @@ export const SortPopup = React.memo(() => {
     <div ref={sortRef} className={styles.sort__wrap} onClick={() => setOpen(!open)}>
       <div className={styles.sort__label}>
         <span>{sort.name}</span>
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-        >
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
+       <svg height="30px" viewBox="0 0 512 512" width="30px">
+        <polygon points="396.6,160 416,180.7 256,352 96,180.7 115.3,160 256,310.5 "/>
         </svg>
       </div>
       {open && (

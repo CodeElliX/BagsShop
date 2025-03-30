b9bc99d47b7d6d7f45b1acd3df173b647e4218f2
ElliX <codeellix@gmail.com>
2025-03-30 22:54:45 +0200

    fix: filters mobile version resized

---
+++ shop/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+'use client'
+import Search from "../../src/app/search/page";
+import styles from "./header.module.css";
+import Link from "next/link";
+import Image from 'next/image'
+import logo from "../../public/logo_2.png";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const pathname = usePathname();
+
+  if (pathname === '/cart/') { return };
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <Image src={logo} alt="logo" className={styles.logo} priority />
+      </Link>
+      <div className={styles.header__right_side}>
+        <Search />
+        <button className={styles.cart_button}>
+          <Link href="/cart">
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+              <rect fill="none" height="256" width="256" />
+              <path d="M184,184H69.8L41.9,30.6A8,8,0,0,0,34.1,24H16" fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="10" />
+              <circle cx="80" cy="204" fill="none" r="20" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="10" />
+              <circle cx="184" cy="204" fill="none" r="20" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="10" />
+              <path d="M62.5,144H188.1a15.9,15.9,0,0,0,15.7-13.1L216,64H48" fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="10" />
+            </svg>
+          </Link>
+        </button>
+      </div>
+      <Image src={'/BagsShop/lozung_2.png'} alt="lozung" className={styles.header__lozung} width={200} height={200} priority />
+    </div>
+  )
+}
+
+export default Header;
